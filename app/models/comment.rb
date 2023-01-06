4c0a687894aad926b6d987b08066696ba3fe4eab
miho0434 <techstudy0434@gmail.com>
2023-01-06 13:48:55 +0900

    commentバリデーション修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -12,4 +12,5 @@ class Comment < ApplicationRecord
     validates :reason_for_retirement 
   end
 
+
 end
--- app/varidators/comment_validator.rb
+++ app/varidators/comment_validator.rb
@@ -1,8 +1,8 @@
 class CommentValidator < ActiveModel::Validator
   def validate(record)
-    items = [record.atmosphere, record.atmosphere, record.gap, record.treatment, record.reason_for_retirement]
+    items = [record.atmosphere, record.growth_potential, record.gap, record.treatment, record.reason_for_retirement]
     return if items.any?(:present?)
-    record.errors[:base] << 'どれか1つの項目は入力してください。'
+    record.errors[:base] << "どれか1つの項目は入力してください。"
   end
   
 end
